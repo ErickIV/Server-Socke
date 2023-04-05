f8f8337188517e7b169a63cd2ca09a8202d471ab
ErickIV <vieiraerick666@gmail.com>
2023-04-04 21:38:28 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,7 +4,9 @@ import java.io.*;
 public class Principal {
     public static void main(String[] args) {
         try {
-            ServerSocket servidor = new ServerSocket(4444);
+            ServerSocket servidor = new ServerSocket(6969);
+            int porta = servidor.getLocalPort();
+            System.out.println("Porta atribuída pelo sistema operacional: " + porta);
             System.out.println("Aguardando cliente!");
             Socket socket = servidor.accept(); // espera
             System.out.println("Chegou o cliente :"
@@ -15,6 +17,7 @@ public class Principal {
             while ((mensagem = in.readLine()) != null) {
                 System.out.println(mensagem);
             }
+            System.out.println("Fim da conexão");
         } catch (IOException e) {
             System.err.println("Problemas de IO");
         }
